201f5f53618c49cc0c2ae50918a0495a7a8c885d
Walter Forkel <walter@forkel.eu>
2015-10-24 23:30:55 +0200

    Helper can accept and reject requests
    Change-Id: I594c26ea624f4f001bf62e944dc8cf7db9284930

--- app/models/helper.rb
+++ app/models/helper.rb
@@ -3,5 +3,23 @@ class Helper < ActiveRecord::Base
   has_and_belongs_to_many :requests, join_table: :request_statuses
 
 
-  
+  def accept(request)
+    # TODO: Should we test if the helper was invited for the request?
+    # Decrease helper score
+    self.score = score / 0.5
+    self.save
+    # Update RequestState
+    helper = self
+    rs = RequestStatus.where {(request_id == request.id) & (helper_id == helper.id)}.first
+    rs.accepted = true
+    rs.save
+  end
+
+  def reject(request)
+    # Set helper status on request
+    helper = self
+    rs = RequestStatus.where {(request_id == request.id) & (helper_id == helper.id)}.first
+    rs.accepted = false
+    rs.save
+  end
 end
