948c76e6ecccac5679e9d0af9db75e7764c7aedc
Anatoly Zelenin <anatoly@zelenin.de>
2015-11-27 22:19:25 +0100

    Removed invitation_key generation
    is done later
    
    Change-Id: I2123d4ab1b01f5fe33d591473a30e7ab27391e1b

--- db/migrate/20151122183211_add_invite_key_to_projects.rb
+++ db/migrate/20151122183211_add_invite_key_to_projects.rb
@@ -2,9 +2,5 @@ class AddInviteKeyToProjects < ActiveRecord::Migration
   require 'securerandom'
   def change
     add_column :projects, :invite_key, :string
-    Project.all.each do |p|
-      p.invite_key = SecureRandom.urlsafe_base64
-      p.save
-    end
   end
 end
